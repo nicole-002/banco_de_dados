6aef1f09dc1361c59bd062c569b72fdcaa00f574
Nicole Modesto <160046955+nicole-002@users.noreply.github.com>
2025-05-21 12:44:17 -0300

    Add files via upload

--- mysql/etecmcm2.sql
+++ mysql/etecmcm2.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 14-Maio-2025 às 17:45
+-- Tempo de geração: 21-Maio-2025 às 17:43
 -- Versão do servidor: 10.4.22-MariaDB
 -- versão do PHP: 8.1.2
 
@@ -59,24 +59,25 @@ CREATE TABLE `produtos` (
   `preco` decimal(10,2) DEFAULT NULL,
   `quant` int(11) DEFAULT NULL,
   `marca` varchar(255) DEFAULT NULL,
-  `id_categoria` int(11) DEFAULT NULL
+  `id_categoria` int(11) DEFAULT NULL,
+  `unidade_medida` varchar(10) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Extraindo dados da tabela `produtos`
 --
 
-INSERT INTO `produtos` (`id_produto`, `nome`, `preco`, `quant`, `marca`, `id_categoria`) VALUES
-(1, 'Sabão em pó', '30.50', 50, 'OMO', 1),
-(2, 'Detergente', '3.50', 10, 'Ypê', 1),
-(4, 'Desinfetante', '10.80', 30, 'Búfalo', 1),
-(5, 'Pasta de dente', '2.50', 120, 'Colgate', 4),
-(6, 'Sabonete líquido', '7.50', 50, 'Lux', 4),
-(7, 'Linguiça temperada', '22.90', 30, 'Sadia', 5),
-(8, 'Costela bovina', '36.90', 30, 'Friboi', 5),
-(9, 'Leite fermentado', '7.90', 40, 'Chamyto', 6),
-(10, 'Iogurte', '12.90', 40, 'Chamyto', 6),
-(11, 'Refrigerante', '10.50', 50, 'Fanta', 7);
+INSERT INTO `produtos` (`id_produto`, `nome`, `preco`, `quant`, `marca`, `id_categoria`, `unidade_medida`) VALUES
+(1, 'Sabão em pó', '30.50', 50, 'OMO', 1, 'un'),
+(2, 'Detergente', '3.50', 10, 'Ypê', 1, 'un'),
+(4, 'Desinfetante', '10.80', 30, 'Búfalo', 1, 'un'),
+(5, 'Pasta de dente', '2.50', 120, 'Colgate', 4, NULL),
+(6, 'Sabonete líquido', '7.50', 50, 'Lux', 4, NULL),
+(7, 'Linguiça temperada', '22.90', 30, 'Sadia', 5, 'kg'),
+(8, 'Costela bovina', '36.90', 30, 'Friboi', 5, 'kg'),
+(9, 'Leite fermentado', '7.90', 40, 'Chamyto', 6, 'ml'),
+(10, 'Iogurte', '12.90', 40, 'Chamyto', 6, 'ml'),
+(11, 'Refrigerante', '10.50', 50, 'Fanta', 7, 'ml');
 
 --
 -- Índices para tabelas despejadas
